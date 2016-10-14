c09ad1948a00fa3ea047d6cf39387d065b1f878d
kkaatii <bulb.dan@gmail.com>
2016-10-14 18:50:48 +0800

    Strip all the redundant parts in v0.0

--- src/main/java/photon/BeanConfig.java
+++ src/main/java/photon/BeanConfig.java
@@ -1,11 +1,5 @@
 package photon;
 
-import java.io.InputStream;
-
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.*;
-import org.mybatis.spring.SqlSessionTemplate;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -13,21 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@MapperScan("photon.data")
 public class BeanConfig {
-
-    @Bean
-    public SqlSessionFactory sqlSessionFactory() throws Exception {
-        String resource = "xml/mybatis/mybatis-config.xml";
-        InputStream inputStream = Resources.getResourceAsStream(resource);
-        return new SqlSessionFactoryBuilder().build(inputStream);
-    }
-
-    @Bean
-    public SqlSessionTemplate sessionTemplate() throws Exception {
-        return new SqlSessionTemplate(sqlSessionFactory());
-    }
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurerAdapter() {
