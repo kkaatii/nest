896e8e62df2a1ddbfe59b6c66ed28df18a5dbc4f
kkaatii <bulb.dan@gmail.com>
2016-10-25 00:53:45 +0800

    Add several users to the white list

--- webapp/routes/auth.js
+++ webapp/routes/auth.js
@@ -1,10 +1,14 @@
 'use strict';
 
 module.exports = function (req, res, next) {
-  if (typeof req.user !== 'undefined' && req.user.id === "windowslive|a0d76fa73ee8c755")
+  if (typeof req.user !== 'undefined' && validUser(req.user))
     next();
   else {
     req.logout();
     res.redirect('/login');
   }
 };
+
+function validUser(user) {
+  return user.id === "windowslive|a0d76fa73ee8c755" || user.nickname === "finn199411" || user.nickname === "bulb.dan" || user.nickname === "evomjv";
+}
