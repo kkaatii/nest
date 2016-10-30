17872204b967b8f4444cf834cc37fac96c479df4
kkaatii <bulb.dan@gmail.com>
2016-10-30 21:12:31 +0800

    Continue optimizing layout

--- webapp/public/js/mfw.js
+++ webapp/public/js/mfw.js
@@ -35,8 +35,8 @@ var ArticleList = React.createClass({
       h.push(
         <div className="row" style={{
           fontSize: "2.1em",
-          marginTop: "0.24em",
-          marginBottom: "0.64em",
+          marginTop: "0.22em",
+          marginBottom: "0.72em",
           paddingLeft: "20%",
           paddingRight: "20%"
         }}>
